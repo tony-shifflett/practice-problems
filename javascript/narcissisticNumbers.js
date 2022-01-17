ed26edd038a865c682b1aa347c5fce8522cb5c62
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-01-17 00:52:55 -0600

    fixed spelling error, isNarcisstic to reflect value of function

---
+++ javascript/narcissisticNumbers.js
@@ -0,0 +1,6 @@
+// code assumes that inputs will be valid, positive integers, and returns a true or false value indicating whether the input meets the definition of a 'narcissistic number', with the value of the nuber being the sum of each digit raised to the power of the number of digits in the number. 
+
+const narcissistic = (value)=>{
+    let isNarcissistic = false; 
+    console.log(isNarcissistic)
+}
