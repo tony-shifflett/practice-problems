d106e848a4b4ba98abfa0a2951a5b46f4cdbd6bc
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-01-17 18:28:03 -0600

    stubbed is_square

---
+++ python/square.py
@@ -0,0 +1,7 @@
+# This function accepts an integer argument and determines whether or not that 
+# integer is the square of an integer, then returns a boolean value indicating whether
+# it is (true) or isn't (false)
+
+def is_square(n):
+    perfectSquare = False
+    return perfectSquare
