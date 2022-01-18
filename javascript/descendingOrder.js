c8b56ad0e8a9f6b07a95f2398e258846093a1582
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-01-17 18:20:39 -0600

    descending order

--- javascript/descendingOrder.js
+++ javascript/descendingOrder.js
@@ -1,4 +1,12 @@
 const descendingOrder = (n)=>{
-    let digits = n.toString().split("")
+    //initialize an empty string to hold output
+    let biggerNum = "";
+    //convert integer to string, split into individual digits, sort them, reverse them, then add them to the empty string via concatenation to form new number
+    let digits = n.toString().split("").sort().reverse().map((num)=>biggerNum+=num)
+    //convert new number to integer from string
+    biggerNum = parseInt(biggerNum) 
+    // return result
+    return biggerNum
 }
 
+descendingOrder(1234)
